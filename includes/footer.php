0e9fbb1563975cb68d6affd9fa4f2f0cbf82f9b4
Lucas Maximiliano Kemerer <lucmaxkemerer@gmail.com>
2026-09-02 18:54:19 -0300

    feat:  header, footer y recursos

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+</main> <!-- Cerramos el main container que abrimos en el header -->
+
+    <!-- Script de Bootstrap (Bundle que incluye Popper para los menú desplegables si los usás después) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <!-- Nuestro script de validación (que vamos a armar en la fase 4) -->
+    <script src="js/validacion.js"></script>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión · dustbnb</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="css/styles.css" rel="stylesheet">
+    <link rel="icon" href="img/logo-sin-nombre.ico" type="image/x-icon">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-white bg-white shadow-sm">
+        <div class="container">
+            
+            <a class="navbar-brand" href="index.php">
+                <img src="img/logo.png" alt="Logo de dustbnb" height="50">
+            </a>
+        </div>
+    </nav>
+    <!-- Container para la pagina (ej. login) -->
+     <!-- Este container se cierra en el footer junto con el body y el html -->
+    <main class="container mt-5">
