b601a75eb4c8113f6b444048b87b199dc001c5e2
Axelirribarren <axelirribarren@gmail.com>
2026-09-07 23:04:58 -0300

    arreglo de estilo y error de scroll

--- procesoLogin.php
+++ procesoLogin.php
@@ -17,7 +17,7 @@ $mensajeExito = '<div>
                             Permitirá controlar la disponibilidad de los departamentos y mantener el registro de huéspedes.
                         </p>
 
-                        <a href="index.php" class="btn btn-danger-outline px-4 py-2">
+                        <a href="index.php" class="btn btn-outline-danger px-4 py-2">
                             Cerrar Sesión
                         </a>
                     </div>';
@@ -281,7 +281,6 @@ require 'includes/header.php';
             </div>
         </div>
     </div>
-</div>
 
 <?php
     require 'includes/footer.php';
