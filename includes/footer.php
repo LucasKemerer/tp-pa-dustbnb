0d126c1ccd36afca51aa84a5bb45a1b29d0a9094
Lucas Maximiliano Kemerer <lucmaxkemerer@gmail.com>
2026-09-07 22:40:58 -0300

    refactor: tarjeta de mensaje facherita

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
 </main> <!-- el main container que se abre en el header -->
 
-    <footer>© 2026 dustbnb</footer>
-
+    <footer class="text-start px-4">© 2026 dustbnb</footer>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/validacion.js"></script>
